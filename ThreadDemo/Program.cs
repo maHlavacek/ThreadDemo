1d78d4185d0e92b2b3bbafe68e0108621f8b0f84
maHlavacek <martin_hlavacek@icloud.com>
2019-11-26 18:12:14 +0100

    First Thread demo

--- ThreadDemo/Program.cs
+++ ThreadDemo/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadDemo
 {
     class Program
     {
+
+        private static int counter;
+
+        public static int Counter
+        {
+            get => counter; 
+            set => counter = value; 
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Thread Demo!");
+            int counts = 1000000;
+            Thread a = new Thread(() =>
+            {
+                for (int i = 0; i < counts; i++)
+                {
+                    Counter = Counter + 1;
+                }
+            });
+            Thread b = new Thread(() =>
+            {
+                for (int i = 0; i < counts; i++)
+                {
+                    Counter = Counter - 1;
+                }
+            });
+            a.Start();
+            b.Start();
+            a.Join();
+            b.Join();
+            Console.WriteLine($"Counter: {Counter}");
         }
     }
 }
